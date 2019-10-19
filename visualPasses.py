bdb75464ec60014765ed355eaf32fecedda4910d
therealsangwoohan <sangw2ohan@gmail.com>
2019-10-19 16:31:25 -0400

    Base of Backend completed

--- visualPasses.py
+++ visualPasses.py
@@ -1,40 +1,59 @@
+import numpy as np
 import pandas as pd
-import sys
 import requests
+import json
 
 
-def visualPasses(satId: int, lat: float, lng: float, alt: float, days: int,
-                 seconds: int, apiKey: str) -> pd.DataFrame:
-    request = requests.get("http://www.n2yo.com/rest/v1/" +
-                           "satellite/visualpasses/" +
-                           satID + "/" +
-                           lat + "/" +
-                           lng + "/" +
-                           alt + "/" +
-                           days + "/" +
-                           seconds + "/&apiKey=" +
-                           apiKey)
-    request_json = request.json()
-    passes = request_json["passes"]
-    d = {}
-    for key in passes[0]:
-        d[key] = tuple(d[key] for d in passes)
-    df = pd.DataFrame.from_dict(d)
-    df = df[["startUTC", "endUTC", "startAz", "endAz", "startEl", "endEl"]]
-    df['startUTC'] = pd.to_datetime(df['startUTC'], unit='s')
-    df['endUTC'] = pd.to_datetime(df['endUTC'], unit='s')
-    df = df.rename(columns={"startUTC": "startTime", "endUTC": "endTIme"})
-    return df
+def visualPasses(satId_list: list, lat: float, lng: float, alt: float,
+                 days: int, seconds: int, apiKey: str) -> pd.DataFrame:
+    bigDF = pd.DataFrame()
+    for satID in satID_list:
+        request = requests.get("http://www.n2yo.com/rest/v1/" +
+                               "satellite/visualpasses/" +
+                               str(satID) + "/" +
+                               str(lat) + "/" +
+                               str(lng) + "/" +
+                               str(alt) + "/" +
+                               str(days) + "/" +
+                               str(seconds) + "/&apiKey=" +
+                               apiKey)
+        request_json = request.json()
+        satname = (request_json["info"])["satname"]
+        try:
+            passes = request_json["passes"]
+        except Exception:
+            print("No passes for:", satname)
+            continue
+        d = {}
+        for key in passes[0]:
+            d[key] = tuple(d[key] for d in passes)
+        df = pd.DataFrame.from_dict(d)
+        df = df[["startUTC", "endUTC", "duration", "startAz", "endAz",
+                 "startEl", "endEl"]]
+        df['startUTC'] = pd.to_datetime(df['startUTC'], unit='s')
+        df['endUTC'] = pd.to_datetime(df['endUTC'], unit='s')
+        df = df.rename(columns={"startUTC": "startTime", "endUTC": "endTime"})
+        df["satname"] = satname
+        df = df.set_index("satname")
+        bigDF = bigDF.append(df)
+    bigDF = bigDF.sort_values('startTime')
+    bigDF = bigDF.reset_index()
+    bigDF['startTime'] = bigDF['startTime'].astype(str)
+    bigDF['endTime'] = bigDF['endTime'].astype(str)
+    return bigDF
 
 
 if __name__ == "__main__":
-    nA = len(sys.argv)
-    satID = sys.argv[nA - 7]
-    lat = sys.argv[nA - 6]
-    lng = sys.argv[nA - 5]
-    alt = sys.argv[nA - 4]
-    days = sys.argv[nA - 3]
-    seconds = sys.argv[nA - 2]
-    apiKey = sys.argv[nA - 1]
-    df = visualPasses(satID, lat, lng, alt, days, seconds, apiKey)
+    df = pd.read_csv("Popid.csv", delimiter=',', header=None)
+# print(df)
+    satID_list = list(np.transpose(df.values)[0])
+    lat = 40.7487727
+    lng = -73.629700
+    alt = 0
+    days = 1
+    seconds = 1
+    apiKey = "HF5J3Q-L52Z93-EBH98V-47RW"
+    df = visualPasses(satID_list, lat, lng, alt, days, seconds, apiKey)
     print(df)
+    parsed = json.loads(df.to_json(orient='index'))
+    print(json.dumps(parsed, indent=4, sort_keys=True))
